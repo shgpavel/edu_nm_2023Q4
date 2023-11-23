7a9eebdaa3d923d13268ee6f4e3f07209776df72
Pavel <shgpavel@yandex.ru>
2023-11-24 02:39:44 +0300

    update 924

--- task_2/MPI/main_MPI.cpp
+++ task_2/MPI/main_MPI.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <string>
+#include <fstream>
 
 #include <eigen3/Eigen/Dense>
 
 
 #include "main.hpp"
 
-#define epsilon 1e-6
-#define N 15
-
-
-typedef enum cases {first, second, third, fourth, fifth} case_t;
+typedef enum cases {first, second} case_t;
 case_t mpi_case = first;
+
 
 Vector::Vector(size_t size) : data(size) {}
 
@@ -55,17 +54,18 @@ const double& Vector::operator[](size_t index) const {
 }
 
 double Vector::norm_diff(const Vector& other) const {
-    double result = 0;
-    for (size_t i = 0; i < this->size(); ++i) {
-        result += pow((*this)[i] - other[i], 2);
+    double result = 0.0;
+    for (size_t i = 0; i < size(); ++i) {
+        result += pow(data[i] - other.data[i], 2);
     }
     return sqrt(result);
 }
 
 void Vector::print() const {
-    for (size_t i = 0; i < this->size(); ++i) {
-        std::cout << (*this)[i] << '\n';
+    for (size_t i = 0; i < size(); ++i) {
+        std::cout << data[i] << ' ';
     }
+    std::cout << '\n';
 }
 
 
@@ -94,13 +94,6 @@ Matrix& Matrix::operator=(const Matrix& other) {
     return *this;
 }
 
-/*
-Matrix Matrix::operator=(std::initializer_list<double>& a) {
-    try ()
-    Matrix result(a.size(), a.size());
-}
-*/
-
 size_t Matrix::rows() const {
     return data.size();
 }
@@ -126,10 +119,10 @@ Vector Matrix::operator*(const Vector& vector) const {
 
 Matrix Matrix::operator*(const Matrix& other) const {
     Matrix result(rows(), other.columns());
-    for (unsigned i = 0; i < rows(); ++i) {
-        for (unsigned j = 0; j < other.columns(); ++j) {
+    for (size_t i = 0; i < rows(); ++i) {
+        for (size_t j = 0; j < other.columns(); ++j) {
             double sum = 0.0;
-            for (unsigned k = 0; k < columns(); ++k) {
+            for (size_t k = 0; k < columns(); ++k) {
                 sum += data[i][k] * other.data[k][j];
             }
             result.data[i][j] = sum;
@@ -139,10 +132,10 @@ Matrix Matrix::operator*(const Matrix& other) const {
 }
 
 Matrix Matrix::transpose() const {
-    Matrix result(*this);
-    for (size_t i = 0; i < this->rows(); ++i) {
-        for (size_t j = 0; j < this->columns(); ++j) {
-            result[i][j] = (*this)[j][i];
+    Matrix result(rows(), columns());
+    for (size_t i = 0; i < rows(); ++i) {
+        for (size_t j = 0; j < columns(); ++j) {
+            result[i][j] = data[j][i];
         }
     }
     return result;
@@ -151,18 +144,17 @@ Matrix Matrix::transpose() const {
 
 double Matrix::norm_inf() const {
     double norm_inf = 0.0;
-    for (size_t i = 0; i < this->rows(); ++i) {
+    for (size_t i = 0; i < rows(); ++i) {
         double row_sum = 0.0;
 
-        for (size_t j = 0; j < this->columns(); ++j) {
-            row_sum += (*this)[i][j];
+        for (size_t j = 0; j < columns(); ++j) {
+            row_sum += std::abs(data[i][j]);
         }
 
         if (row_sum > norm_inf) {
             norm_inf = row_sum;
         }
     }
-
     return norm_inf;
 }
 
@@ -174,18 +166,36 @@ double Matrix::norm_1() const {
 
 
 void Matrix::print() const {
-    for (size_t i = 0; i < this->rows(); ++i) {
-        for (size_t j = 0; j < this->columns(); ++j) {
-            std::cout << (*this)[i][j] << " ";
+    for (size_t i = 0; i < rows(); ++i) {
+        for (size_t j = 0; j < columns(); ++j) {
+            std::cout << data[i][j] << " ";
         }
         std::cout << '\n';
     } 
 }
 
-unsigned MPI(Matrix& M, Vector& b, Vector& solution) {
-    unsigned k = 1;
+/* Here I will destroy race condition
+Matrix MPI_aux(Matrix M) {
+    Matrix B(M);
     double mi = 1.0/M.norm_inf();
-    
+    for (size_t i = 0; i < B.rows(); ++i) {
+        for (size_t j = 0; j < B.columns(); ++j) {
+            if (i == j) {
+                B[i][j] = 1 - (mi * B[i][j]);
+            } else {
+                B[i][j] = -mi * B[i][j];
+            }
+        }
+    }
+
+    return B;
+}
+*/
+
+
+void MPI(Matrix& M, Vector& b, Vector& solution, double epsilon) {
+    double mi = 1.0/M.norm_inf();
+
     Matrix B(M);
     for (size_t i = 0; i < B.rows(); ++i) {
         for (size_t j = 0; j < B.columns(); ++j) {
@@ -196,9 +206,8 @@ unsigned MPI(Matrix& M, Vector& b, Vector& solution) {
             }
         }
     }
-    
 
-    if ( B.norm_inf() >= 1 && B.norm_1() >= 1 ) {
+    if ( B.norm_inf() >= 1.0 && B.norm_1() >= 1.0 ) {
         if ( mpi_case == first ) {
             Matrix M_trs(M);
             Vector cache(b.size());
@@ -206,12 +215,13 @@ unsigned MPI(Matrix& M, Vector& b, Vector& solution) {
             M_trs = M.transpose();
             cache = b;
 
-            M = M * M_trs;
+            M = M_trs * M;
             b = M_trs * cache;
 
             mpi_case = second;
-            k = MPI(M, b, solution);
-            return k;
+            MPI(M, b, solution, epsilon);
+            return;
+        
         } else if ( mpi_case == second ) {
             Vector c(b.size());
             c = b;
@@ -224,21 +234,17 @@ unsigned MPI(Matrix& M, Vector& b, Vector& solution) {
             Vector cache(c.size());
             
             x_cur = c;
-            cache = B * x_cur;
-            x_next = cache + c;
+            x_next = B * x_cur + c;
             cache = M * x_next;
             
             while (cache.norm_diff(b) > epsilon) {
                 x_cur = x_next;
-                cache = B * x_cur;
-                x_next = cache + c;
-                ++k;
+                x_next = B * x_cur + c;
                 cache = M * x_next;
             }
 
             solution = x_next;
-            return k;
- 
+            return;
         }
     }
 
@@ -249,22 +255,18 @@ unsigned MPI(Matrix& M, Vector& b, Vector& solution) {
     }
     
 
+    double stop_mult = B.norm_inf() / (1.0 - B.norm_inf());
     Vector x_cur(c.size());
     Vector x_next(c.size());
-    Vector cache(c.size());
-    double stop_mult = B.norm_inf() / (1 - B.norm_inf());
 
 
-    x_cur = c;
+    x_next = c;
     do {
-        cache = x_cur;
         x_cur = x_next;
-        x_next = B * cache + c;
-        ++k;
+        x_next = B * x_cur + c;
     } while ((stop_mult * x_next.norm_diff(x_cur)) > epsilon);
 
     solution = x_next;
-    return k;
 }
 
 
@@ -290,67 +292,73 @@ void eigen_solve(Matrix& A, Vector& b, Vector& solution) {
     }
 }
 
-void test(case_t num, Matrix& M, Vector& b) {
-    switch (num) {
-        case first:
-            M = { 0, 2, 3, 1, 2, 4, 4, 5, 6 };
-            b = { 13, 17, 32 };
+void parse_test(std::string filename) {
+    std::ifstream ifile(filename, std::ios::binary);
+
+    if ( !ifile.is_open() ) {
+        throw std::invalid_argument("No such file");
     }
+
+    std::cout.precision(8); 
+    while (1) {
+        double epsilon;
+        size_t matrix_size;
+
+        ifile >> epsilon >> matrix_size;
+
+        if (ifile.eof()) break;
+
+        Matrix M(matrix_size, matrix_size);
+        Vector b(matrix_size);
+        Vector solution(matrix_size);
+
+        for (size_t i = 0; i < matrix_size; ++i) {
+            for (size_t j = 0; j < matrix_size; ++j) {
+                ifile >> M[i][j];
+            }
+        }
+
+        for (size_t i = 0; i < matrix_size; ++i) {
+            ifile >> b[i];
+        }
+
+        M.print();
+        b.print();
+
+        eigen_solve(M, b, solution);
+        std::cout << "Eigen:" << '\n';
+        solution.print();
+
+        std::cout << '\n';
+
+        MPI(M, b, solution, epsilon); 
+        std::cout << "MPI:" << '\n';
+        solution.print();
+        
+        std::cout << "\n\n";
+    }
+
+    if (ifile.bad()) {
+        throw std::runtime_error("Could not read from this file");
+    }
+
+    ifile.close();
 }
+
 
 int main(void) {
-    
-    /*
-    size_t n;
-    std::cout << "Give me matrix dismension:"<< '\n';
-    
-    while (1) {
+
+    while(1) {
         try {
-        std::cin >> n;
-        std::cout << '\n';
-        
-        if (n < 1) {
-            throw std::invalid_argument("Matrix size must be >= 1 1;");
-        } else {
+            std::string filename;
+            std::cout << "Give me filename: ";
+            std::cin >> filename;
+            parse_test(filename);
             break;
-        }
         } catch (const std::invalid_argument& e) {
-            std::cerr << "Error: " << e.what() << ";" << '\n';
+            std::cerr << "Error: " << e.what() << '\n';
         }
     }
-
-    Matrix m10(n, n);
-    std::cout << "Give me elements of A matrix:" << '\n';
-    for (size_t i = 0; i < m10.rows(); ++i) {
-        for (size_t j = 0; j < m10.columns(); ++j) {
-            std::cin >> m10[i][j];
-        }
-    }
-    std::cout << '\n';
-
-    Vector v1(n);
-    std::cout << "Give me coordinates of b vector:" << '\n';
-    for (size_t i = 0; i < v1.size(); ++i) {
-        std::cin >> v1[i];
-    }
-    std::cout << '\n';
-
-     Data is set 
     
-    Matrix m10(3, 3);
-    Vector v1(3);
-    Vector solution(v1.size());
-
-    test(first, m10, v1);
-    eigen_solve(m10, v1, solution);
-    std::cout << "Eigen:" << '\n';
-    solution.print();
-
-    unsigned g = MPI(m10, v1, solution);
-    std::cout << "MPI:" << '\n';
-    solution.print();
-    std::cout << "Number of iters: " << g << std::endl;
-    */
     return 0;
 }
-
